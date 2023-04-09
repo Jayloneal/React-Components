76ba3eafd418929a84a7da6e8520cf767601f3cf
Jayloneal <121124684+Jayloneal@users.noreply.github.com>
2023-04-09 09:13:49 -0400

    Add files via upload

---
+++ Lists.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function Lists() {
+  <ul>
+    <li>Bacon</li>
+    <li>Jamon</li>
+    <li>Noodles</li>
+  </ul>;
+}
+export default Lists;
